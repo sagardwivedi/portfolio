e335681b3c1d1b2595ab742770f4c2e3aae9ba69
sagardwivedi <sagar.dw20@gmail.com>
2023-12-09 19:33:40 +0530

    Setup and integrating with sanity

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+export function Footer() {
+  return (
+    <div className="flex flex-row justify-center items-center">
+      <p></p>
+      <p></p>
+    </div>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+export function Navbar() {
+    return (
+      <div className="flex flex-row justify-center items-center">
+        <p></p>
+        <p></p>
+      </div>
+    );
+  }
+  
