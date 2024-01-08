1142c5e90c47ee8dafdf4897f497f122812bc546
sagardwivedi <sagar.dw20@gmail.com>
2024-01-08 19:27:20 +0530

    Fix CSS and layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Home = () => {
   return (
-    <div className="base flex min-h-[40rem] flex-row items-center justify-center gap-10">
+    <div className="base flex min-h-[40rem] flex-row max-md:flex-col items-center justify-center gap-10">
       <Avatar className="mb-5 h-40 w-40 border-4 border-primary-foreground">
         <AvatarImage
           src="https://github.com/sagardwivedi.png"
